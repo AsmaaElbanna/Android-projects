3b7757fb315a5cf8ba04a1406d30c478f63faf11
moutaz-hegazy <moutazhegazy93@gmail.com>
2021-03-20 00:27:12 +0200

    navigation drawer done

---
+++ AndroidProject/app/src/main/java/com/example/androidproject/navigation_drawer_activity/ui/upcoming/UpcomingFragment.java
@@ -0,0 +1,38 @@
+package com.example.androidproject.navigation_drawer_activity.ui.upcoming;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.androidproject.R;
+
+public class UpcomingFragment extends Fragment {
+
+    private UpcomingViewModel mViewModel;
+
+    public static UpcomingFragment newInstance() {
+        return new UpcomingFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.upcoming_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(UpcomingViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ AndroidProject/app/src/main/java/com/example/androidproject/navigation_drawer_activity/ui/upcoming/UpcomingViewModel.java
@@ -0,0 +1,7 @@
+package com.example.androidproject.navigation_drawer_activity.ui.upcoming;
+
+import androidx.lifecycle.ViewModel;
+
+public class UpcomingViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
